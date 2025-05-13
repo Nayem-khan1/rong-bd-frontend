2caf19ff45be868660f37fa907a7c619187c862d
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-05-13 17:54:05 +0600

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Order from './pages/Order'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Searchbar from './components/Searchbar'
+import { Toaster } from 'react-hot-toast';
+import Cart from './pages/Cart'
+
+function App() {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Toaster position="top-right" reverseOrder={false} />
+      <Navbar/>
+      <Searchbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Order/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
